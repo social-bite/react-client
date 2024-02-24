62bbcf795892c4e45ccffd546f53815b1049d241
hustincao <40309113+hustincao@users.noreply.github.com>
2024-02-23 22:48:21 -0500

    moved login and register code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,43 @@
 export default function Login() {
-    return (
-      <>Login</>
-    );
-  }
+  return (
+    <section>
+      <div>
+        <h1 class="pb-4">Login</h1>
+     
+        <form class="mb-4">
+          <div class="flex flex-col w-6/12">
+            <label for="Username">Username</label>
+            <input
+              class="border-solid border-2 border-[black]"
+              type="text"
+              name="Username"
+            />
+          </div>
+          <div class="flex flex-col w-6/12">
+            <label for="password">Password</label>
+            <input
+              class="border-solid border-2 border-[black]"
+              type="text"
+              name="password"
+            />
+          </div>
+          <button
+            class="mt-4 font-[bold] w-full max-w-[15rem] text-xl transition-all duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[black]"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+        <div class="flex">
+          <h4 class="mr-[0.25rem]">Don't have an account?</h4>
+          <a
+            class="hover:text-blue-700 transition-all duration-300 ease-[ease]"
+            href="/signup"
+          >
+            Sign up!
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,92 @@
+import { useState } from 'react';
+import { API_URL } from '../utils/utils';
+
 export default function Signup() {
-    return (
-      <>Register</>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  let minLength = 8;
+  let maxLength = 72
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (password !== passwordConfirm) {
+        alert("Passwords do not match.");
+        return;
+    }
+    if (password.length < minLength || password.length > maxLength) {
+      alert("Password must be over 8 characters and less than 72 characters");
+      return;
+    }
+    try {
+      const response = await fetch(API_URL + '/api/accounts/register/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password, passwordConfirm })
+      });
+      console.log(response);
+
+      if (response.ok) {
+        // TODO: Navigate to /feed
+        // navigate('/feed')
+        
+      } 
+      else {
+        alert('Failed to register account. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error registering account:', error);
+      alert('An error occurred. Please try again later.');
+    }
   }
+
+
+  return (
+    <section>
+      <div>
+        <h1 className="pb-4">Signup</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col w-6/12">
+            <label htmlFor="username">Username</label>
+            <input
+              className="border-solid border-2 border-[black]"
+              type="text"
+              name="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col w-6/12">
+            <label htmlFor="password">Password</label>
+            <input
+              className="border-solid border-2 border-[black]"
+              type="text"
+              name="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col w-6/12">
+            <label htmlFor="passwordConfirm">Confirm Password</label>
+            <input
+              className="border-solid border-2 border-[black]"
+              type="text"
+              name="passwordConfirm"
+              value={passwordConfirm}
+              onChange={e => setPasswordConfirm(e.target.value)}
+            />
+          </div>
+          <button
+            className="mt-4 font-[bold] w-full max-w-[15rem] text-xl transition-all duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[black]"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
