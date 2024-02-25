6bacaced77377cebad77e0cc39db2506bcc68166
hustincao <40309113+hustincao@users.noreply.github.com>
2024-02-25 14:09:42 -0500

    code cleanup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import { API_URL } from '../utils/utils';
-import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/socialbite.svg';
+import { API_URL } from "../utils/utils";
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/socialbite.svg";
 
 function Login() {
   const navigate = useNavigate();
   const [login, setLogin] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
 
+  const [errorMessage, setErrorMessage] = useState("");
+
   function handleNotification(notification) {
-    let parent = document.getElementById('parent_notification');
-    parent.innerHTML = '';
-    parent.innerHTML += notification;
+    // let parent = document.getElementById("parent_notification");
+    // parent.innerHTML = "";
+    // parent.innerHTML += notification;
+    setErrorMessage(notification);
   }
 
   async function handleSubmit(event) {
@@ -26,70 +29,58 @@ function Login() {
       return;
     }
     try {
-      const response = await fetch(API_URL + '/api/accounts/login/', {
-        method: 'POST',
+      const response = await fetch(API_URL + "/api/accounts/login/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ username, password }),
       });
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       if (response.ok) {
-        navigate('/feed');
-      }
-      else {
+        navigate("/feed");
+      } else {
         handleNotification(data["message"]);
       }
-    } catch(error) {
-      console.error('Server error', error);
-      handleNotification("Server error")
+    } catch (error) {
+      console.error("Server error", error);
+      handleNotification("Server error");
     }
   }
 
   return (
     <section className="h-screen w-screen flex flex-col justify-center items-center bg-black text-white">
-      <img src={Logo} alt='SocialBite Logo' className='pb-4' />
-      <div id='parent_notification'></div>
-      <form class="mb-4 w-1/2" onSubmit={event => handleSubmit(event)}>
-        <div class="flex flex-col">  
+      <img src={Logo} alt="SocialBite Logo" className="pb-4" />
+      <div id="parent_notification">{errorMessage ? errorMessage : ""}</div>
+      <form className="mb-4 w-1/2" onSubmit={(event) => handleSubmit(event)}>
+        <div className="flex flex-col">
           <input
-            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
-              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
-              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            className="input-minimal"
             type="text"
             name="Username"
             value={login.username}
-            placeholder='Username'
-            onChange={e => setLogin({...login, username: (e.target.value)})}
+            placeholder="Username"
+            onChange={(e) => setLogin({ ...login, username: e.target.value })}
           />
         </div>
-        <div class="flex flex-col">
+        <div className="flex flex-col">
           <input
-            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
-              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
-              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            className="input-minimal"
             type="text"
             name="password"
             value={login.password}
-            placeholder='Password'
-            onChange={e => setLogin({...login, password: (e.target.value)})}
+            placeholder="Password"
+            onChange={(e) => setLogin({ ...login, password: e.target.value })}
           />
         </div>
-        <button
-          class="bg-[#3B836E] mt-4 mb-4 font-[bold] w-full max-w-[15rem] text-xl transition-all 
-            duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[#3B836E] rounded-md"
-          type="submit"
-        > 
+        <button className="btn-teal" type="submit">
           Login
         </button>
       </form>
-      <div class="flex">
-        <h4 class="mr-[0.25rem]">Don't have an account?</h4>
-        <a
-          class="hover:text-blue-700 transition-all duration-300 ease-[ease] hover:text-[#3B836E]"
-          href="/register"
-        >
+      <div className="flex">
+        Don't have an account?
+        <a className="ml-1 link-orange" href="/register">
           Register!
         </a>
       </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import { useState } from 'react';
-import { API_URL } from '../utils/utils';
-import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/socialbite.svg';
+import React from "react";
+import { useState } from "react";
+import { API_URL } from "../utils/utils";
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/socialbite.svg";
 
 function Signup() {
   const navigate = useNavigate();
   const [login, setLogin] = useState({
-    username: '',
-    password: '',
-    passwordConfirm: '',
+    username: "",
+    password: "",
+    passwordConfirm: "",
   });
 
+  const [errorMessage, setErrorMessage] = useState("");
+
   function handleNotification(notification) {
-    let parent = document.getElementById('parent_notification');
-    parent.innerHTML = '';
-    parent.innerHTML += notification;
+    // let parent = document.getElementById("parent_notification");
+    // parent.innerHTML = "";
+    // parent.innerHTML += notification;
+    setErrorMessage(notification);
+
   }
 
   async function handleSubmit(event) {
@@ -30,90 +34,80 @@ function Signup() {
       return;
     }
     if (password.length < 8 || password.length > 72) {
-      handleNotification("Password must be over 8 characters and less than 72 characters");
+      handleNotification(
+        "Password must be over 8 characters and less than 72 characters"
+      );
       return;
     }
     try {
-      const response = await fetch(API_URL + '/api/accounts/register/', {
-        method: 'POST',
+      const response = await fetch(API_URL + "/api/accounts/register/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password, passwordConfirm })
+        body: JSON.stringify({ username, password, passwordConfirm }),
       });
       const data = await response.json();
       if (response.ok) {
-        navigate('/feed');
-      } 
-      else {
+        navigate("/feed");
+      } else {
         if (data["error"] !== undefined) {
-          // key exists, print error 
-          const jsonObject = JSON.parse(data["error"])
-          console.log(jsonObject)
+          // key exists, print error
+          const jsonObject = JSON.parse(data["error"]);
+          console.log(jsonObject);
           handleNotification(jsonObject["data"]["username"]["message"]);
         }
       }
     } catch (error) {
-      console.error('Error registering account:', error);
+      console.error("Error registering account:", error);
       handleNotification("An error occurred. Please try again later.");
     }
   }
 
   return (
-    <section className='h-screen w-screen flex flex-col justify-center items-center bg-black text-white'>
-      <img src={Logo} alt='SocialBite Logo' className='pb-4' />
-      <div id='parent_notification'></div>
-      <form className='mb-4 w-1/2' onSubmit={handleSubmit}>
+    <section className="h-screen w-screen flex flex-col justify-center items-center bg-black text-white">
+      <img src={Logo} alt="SocialBite Logo" className="pb-4" />
+      <div id="parent_notification">{errorMessage ? errorMessage : ""}</div>
+      <form className="mb-4 w-1/2" onSubmit={handleSubmit}>
         <div className="flex flex-col">
           <input
-            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
-              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
-              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            className="input-minimal"
             type="text"
             name="username"
-            placeholder='Username'
+            placeholder="Username"
             value={login.username}
-            onChange={e => setLogin({...login, username: (e.target.value)})}
+            onChange={(e) => setLogin({ ...login, username: e.target.value })}
           />
         </div>
         <div className="flex flex-col">
           <input
-            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
-              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
-              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            className="input-minimal"
             type="text"
             name="password"
-            placeholder='Password'
+            placeholder="Password"
             value={login.password}
-            onChange={e => setLogin({...login, password: (e.target.value)})}
+            onChange={(e) => setLogin({ ...login, password: e.target.value })}
           />
         </div>
         <div className="flex flex-col">
           <input
-            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
-              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
-              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            className="input-minimal"
             type="text"
             name="passwordConfirm"
-            placeholder='Confirm Password'
+            placeholder="Confirm Password"
             value={login.passwordConfirm}
-            onChange={e => setLogin({...login, passwordConfirm: (e.target.value)})}
+            onChange={(e) =>
+              setLogin({ ...login, passwordConfirm: e.target.value })
+            }
           />
         </div>
-        <button
-        class="bg-[#3B836E] mt-4 mb-4 font-[bold] w-full max-w-[15rem] text-xl transition-all 
-          duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[#3B836E] rounded-md"
-          type="submit"
-        >
+        <button className="btn-teal" type="submit">
           Register
         </button>
       </form>
-      <div class="flex">
-        <h4 class="mr-[0.25rem]">Already have an account?</h4>
-        <a
-          class="hover:text-blue-700 transition-all duration-300 ease-[ease] hover:text-[#3B836E]"
-          href="/login"
-        >
+      <div className="flex">
+        Already have an account?
+        <a className="ml-1 link-orange" href="/login">
           Login
         </a>
       </div>
@@ -121,4 +115,4 @@ function Signup() {
   );
 }
 
-export default Signup
+export default Signup;
