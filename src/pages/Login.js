a8e3014b14b84d37ecc2583d184afbeec38e0e6c
leohchoi <choi.leo2002@gmail.com>
2024-02-25 00:03:05 -0400

    finished ui 2.0 for login and register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from "react";
 import { API_URL } from '../utils/utils';
 import { useNavigate } from 'react-router-dom';
+import Logo from '../assets/socialbite.svg';
 
 function Login() {
   const navigate = useNavigate();
@@ -47,47 +48,50 @@ function Login() {
   }
 
   return (
-    <section>
-      <div>
-        <h1 class="pb-4">Login</h1>
-        <div id='parent_notification'></div>
-        <form class="mb-4" onSubmit={event => handleSubmit(event)}>
-          <div class="flex flex-col w-6/12">
-            <label for="Username">Username</label>
-            <input
-              class="border-solid border-2 border-[black]"
-              type="text"
-              name="Username"
-              value={login.username}
-              onChange={e => setLogin({...login, username: (e.target.value)})}
-            />
-          </div>
-          <div class="flex flex-col w-6/12">
-            <label for="password">Password</label>
-            <input
-              class="border-solid border-2 border-[black]"
-              type="text"
-              name="password"
-              value={login.password}
-              onChange={e => setLogin({...login, password: (e.target.value)})}
-            />
-          </div>
-          <button
-            class="mt-4 font-[bold] w-full max-w-[15rem] text-xl transition-all duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[black]"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-        <div class="flex">
-          <h4 class="mr-[0.25rem]">Don't have an account?</h4>
-          <a
-            class="hover:text-blue-700 transition-all duration-300 ease-[ease]"
-            href="/register"
-          >
-            Register!
-          </a>
+    <section className="h-screen w-screen flex flex-col justify-center items-center bg-black text-white">
+      <img src={Logo} alt='SocialBite Logo' className='pb-4' />
+      <div id='parent_notification'></div>
+      <form class="mb-4 w-1/2" onSubmit={event => handleSubmit(event)}>
+        <div class="flex flex-col">  
+          <input
+            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
+              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
+              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            type="text"
+            name="Username"
+            value={login.username}
+            placeholder='Username'
+            onChange={e => setLogin({...login, username: (e.target.value)})}
+          />
         </div>
+        <div class="flex flex-col">
+          <input
+            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
+              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
+              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            type="text"
+            name="password"
+            value={login.password}
+            placeholder='Password'
+            onChange={e => setLogin({...login, password: (e.target.value)})}
+          />
+        </div>
+        <button
+          class="bg-[#3B836E] mt-4 mb-4 font-[bold] w-full max-w-[15rem] text-xl transition-all 
+            duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[#3B836E] rounded-md"
+          type="submit"
+        > 
+          Login
+        </button>
+      </form>
+      <div class="flex">
+        <h4 class="mr-[0.25rem]">Don't have an account?</h4>
+        <a
+          class="hover:text-blue-700 transition-all duration-300 ease-[ease] hover:text-[#3B836E]"
+          href="/register"
+        >
+          Register!
+        </a>
       </div>
     </section>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { API_URL } from '../utils/utils';
 import { useNavigate } from 'react-router-dom';
+import Logo from '../assets/socialbite.svg';
 
 function Signup() {
   const navigate = useNavigate();
@@ -59,48 +60,62 @@ function Signup() {
   }
 
   return (
-    <section>
-      <div>
-        <h1 className="pb-4">Register</h1>
-        <div id='parent_notification'></div>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col w-6/12">
-            <label htmlFor="username">Username</label>
-            <input
-              className="border-solid border-2 border-[black]"
-              type="text"
-              name="username"
-              value={login.username}
-              onChange={e => setLogin({...login, username: (e.target.value)})}
-            />
-          </div>
-          <div className="flex flex-col w-6/12">
-            <label htmlFor="password">Password</label>
-            <input
-              className="border-solid border-2 border-[black]"
-              type="text"
-              name="password"
-              value={login.password}
-              onChange={e => setLogin({...login, password: (e.target.value)})}
-            />
-          </div>
-          <div className="flex flex-col w-6/12">
-            <label htmlFor="passwordConfirm">Confirm Password</label>
-            <input
-              className="border-solid border-2 border-[black]"
-              type="text"
-              name="passwordConfirm"
-              value={login.passwordConfirm}
-              onChange={e => setLogin({...login, passwordConfirm: (e.target.value)})}
-            />
-          </div>
-          <button
-            className="mt-4 font-[bold] w-full max-w-[15rem] text-xl transition-all duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[black]"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
+    <section className='h-screen w-screen flex flex-col justify-center items-center bg-black text-white'>
+      <img src={Logo} alt='SocialBite Logo' className='pb-4' />
+      <div id='parent_notification'></div>
+      <form className='mb-4 w-1/2' onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+          <input
+            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
+              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
+              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            type="text"
+            name="username"
+            placeholder='Username'
+            value={login.username}
+            onChange={e => setLogin({...login, username: (e.target.value)})}
+          />
+        </div>
+        <div className="flex flex-col">
+          <input
+            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
+              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
+              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            type="text"
+            name="password"
+            placeholder='Password'
+            value={login.password}
+            onChange={e => setLogin({...login, password: (e.target.value)})}
+          />
+        </div>
+        <div className="flex flex-col">
+          <input
+            class="w-full bg-[black] text-[white] outline-none border-t-[none] border-b-[0.1rem]
+              border-x-[none] mb-4 transition-all duration-200 ease-[ease]
+              hover:border-x-[none] hover:border-t-[none] hover:border-[#3B836E] hover:border-b-[0.2rem]"
+            type="text"
+            name="passwordConfirm"
+            placeholder='Confirm Password'
+            value={login.passwordConfirm}
+            onChange={e => setLogin({...login, passwordConfirm: (e.target.value)})}
+          />
+        </div>
+        <button
+        class="bg-[#3B836E] mt-4 mb-4 font-[bold] w-full max-w-[15rem] text-xl transition-all 
+          duration-300 ease-[ease] px-6 py-3 border-2 border-solid border-[#3B836E] rounded-md"
+          type="submit"
+        >
+          Register
+        </button>
+      </form>
+      <div class="flex">
+        <h4 class="mr-[0.25rem]">Already have an account?</h4>
+        <a
+          class="hover:text-blue-700 transition-all duration-300 ease-[ease] hover:text-[#3B836E]"
+          href="/login"
+        >
+          Login
+        </a>
       </div>
     </section>
   );
