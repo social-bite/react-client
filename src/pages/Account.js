1dc37cf43f44d86b1978c93ae61096f29489263d
leohchoi <choi.leo2002@gmail.com>
2024-03-01 12:53:02 -0500

    icons to the bottom

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,5 +1,15 @@
-export default function Account() {
-    return (
-      <></>
-    );
-  }
+import React from "react";
+// import { useState } from "react";
+// import { API_URL } from "../utils/utils";
+// import { useNavigate } from "react-router-dom";
+import Logo from "../assets/socialbite_horizontal.svg";
+
+function Account() {
+  return (
+    <div>
+      <img src={Logo} alt="Horizontal Logo"/>
+    </div>
+  );
+}
+
+export default Account
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <div className="h-screen w-screen flex flex-col bg-black text-white">
       <Outlet />
-      <div className="flex justify-between">
+      <div className="w-full fixed bottom-0 left-0 flex justify-between">
         <Link className="w-1/3 flex justify-center" to={"/"}><img className="w-1/3" src={Home} alt="Home icon"/></Link>
         <Link className="w-1/3 flex justify-center" to={"/feed"}><img className="w-1/3" src={Search} alt="Home icon"/></Link>
         <Link className="w-1/3 flex justify-center" to={"/account"}><img className="w-1/3" src={Account} alt="Home icon"/></Link>
